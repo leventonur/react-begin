8d2bf1b7bc90b77a5581d19a068eb9c5f8155ea9
Levent Onur <IS97689@gm.isbank.com.tr>
2022-11-21 22:18:29 +0300

    Levent React İlk Projem

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
+//react'ın çalışmaya başladığı dosya 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//jsx java script söz dizimi ekleme klasik java scripte xml benzerli şablon ekliyoruz
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+function App() {
+    return (
+    <div>
+       
+    </div>
+    );
+    
+} 
+
+ReactDOM.render(
+    <App/>,
+     document.getElementById('root') 
+     
+)
+
+
